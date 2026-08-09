e4b8cfc03802487af9cf6b74bcbbd776dac9355e
Mob <mobinastaraki@gmail.com>
2026-08-09 23:26:06 +0430

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { useQuery } from "@tanstack/react-query"
 
 export default function App() {
 
-  const { data: catfact, isLoading, isError, error, refetch } = useQuery({
+  const { data: catfact, isLoading, isError, error, isFetching, refetch } = useQuery({
     queryKey: ['catfact'],
     queryFn: async () => {
       const response = await axios.get("https://catfact.ninja/fact")
       return response.data
-    }
+    },
+             refetchOnWindowFocus: false,
+             refetchInterval: false,
+             refetchOnReconnect: false,
   })
-
+ 
   if (isLoading) {
     return (
       <div className="bg-app flex items-center justify-center">
-        <p className="text-sm text-zinc-500 tracking-[0.2em] uppercase">
+        <p className="text-sm text-zinc-500 tracking-[0.4em] uppercase">
           Loading...
         </p>
       </div>
@@ -34,13 +37,13 @@ export default function App() {
 
       <div className="relative z-10 w-full max-w-lg px-8 py-24">
 
-        <p className="text-xs text-zinc-500 tracking-[0.3em] uppercase mb-4 text-center">
+        <p className="text-xs flex justify-center text-zinc-500 tracking-[0.3em] uppercase mb-6 text-center">
           Random Facts About Cats
         </p>
 
         <h1
           className="text-7xl font-black text-white mb-8 text-center leading-[0.85]"
-          style={{ fontFamily: "'Bangers', cursive", letterSpacing: '0.05em' }}
+          style={{ fontFamily: "'Bangers', cursive",  letterSpacing: '0.09em' }}
         >
           CAT FACT
         </h1>
@@ -59,9 +62,9 @@ export default function App() {
         <div className="flex flex-col items-center gap-8">
           <button
             onClick={() => refetch()}
-            className="relative px-6 py-3 bg-cyan-500 text-black text-sm font-bold rounded-xl hover:bg-cyan-400 transition-colors tracking-[0.05em] uppercase"
+            className="relative px-6 py-3 bg-cyan-500 text-black text-sm font-bold rounded-xl hover:bg-cyan-400 transition-colors tracking-wider uppercase"
           >
-            <span className="relative z-10">New Fact</span>
+            <span className="relative z-10">{isFetching? "Fetching..." : "New Fact"}</span>
           </button>
 
           <span className="text-xs text-zinc-600 tracking-[0.15em] uppercase">
